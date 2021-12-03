615d12821b62ad79cd29d63fb661f96bfd0b1784
Shridhar <36041359+shridharMe@users.noreply.github.com>
2021-12-03 16:15:53 +0000

    Create pipelineJob.groovy

---
+++ pipelineJob.groovy
@@ -0,0 +1,32 @@
+pipelineJob('rails-mysql-app') {
+  definition {
+    cpsScmFlowDefinition {
+      scm {
+        gitSCM {
+          userRemoteConfigs {
+            userRemoteConfig {
+              credentialsId('shridharMe')
+              name('')
+              refspec('')
+              url('git@github.com:shridharMe/rails-mysql-app.git')
+            }
+          }
+          branches {
+            branchSpec {
+              name('*/master')
+            }
+          }
+          browser {
+            gitWeb {
+              repoUrl('')
+            }
+          }
+          gitTool('')
+          doGenerateSubmoduleConfigurations(false)
+        }
+      }
+      scriptPath('Jenkinsfile')
+      lightweight(true)
+    }
+  }
+}
