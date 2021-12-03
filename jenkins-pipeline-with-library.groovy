b85eaae4e4cba6e967aa9226785b6f7e7d43b9b0
Shridhar <36041359+shridharMe@users.noreply.github.com>
2021-12-03 16:45:57 +0000

    Update jenkins-pipeline-with-library.groovy

--- jenkins-pipeline-with-library.groovy
+++ jenkins-pipeline-with-library.groovy
@@ -1,1 +1,33 @@
 
+pipelineJob('jenkins-pipeline-with-library') {
+  definition {
+    cpsScmFlowDefinition {
+      scm {
+        gitSCM {
+          userRemoteConfigs {
+            userRemoteConfig {
+              credentialsId('shridharMe')
+              name('')
+              refspec('')
+              url('git@github.com:shridharMe/jenkins-pipeline-with-library.git')
+            }
+          }
+          branches {
+            branchSpec {
+              name('*/master')
+            }
+          }
+          browser {
+            gitWeb {
+              repoUrl('')
+            }
+          }
+          gitTool('')
+          doGenerateSubmoduleConfigurations(false)
+        }
+      }
+      scriptPath('Jenkinsfile')
+      lightweight(true)
+    }
+  }
+}
