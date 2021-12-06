6bd2a41f2376e9c4ec96cf30d96a24315ea1bc93
Shridhar <36041359+shridharMe@users.noreply.github.com>
2021-12-06 14:49:51 +0000

    Update jenkinsPipelineWithLibraryPR.groovy

--- jenkinsPipelineWithLibraryPR.groovy
+++ jenkinsPipelineWithLibraryPR.groovy
@@ -1,1 +1,34 @@
 
+
+pipelineJob('build-jenkins-pipeline-with-library') {
+  definition {
+    cpsScmFlowDefinition {
+      scm {
+        gitSCM {
+          userRemoteConfigs {
+            userRemoteConfig {
+              credentialsId('guthub_repo_access')
+              name('')
+              refspec('')
+              url('git@github.com:shridharMe/jenkins-pipeline-with-library.git')
+            }
+          }
+          branches {
+            branchSpec {
+              name('*/master')
+            }
+          }
+          browser {
+            gitWeb {
+              repoUrl('')
+            }
+          }
+          gitTool('')
+          doGenerateSubmoduleConfigurations(false)
+        }
+      }
+      scriptPath('Jenkinsfile')
+      lightweight(true)
+    }
+  }
+}
