949928e2c78fa78a20c5c86e5537e6e0d0619156
Shridhar <36041359+shridharMe@users.noreply.github.com>
2021-12-06 12:34:12 +0000

    Update railsMysqlAppPR.groovy

--- railsMysqlAppPR.groovy
+++ railsMysqlAppPR.groovy
@@ -1,1 +1,33 @@
 
+pipelineJob('rails-mysql-app/PR') {
+  definition {
+    cpsScmFlowDefinition {
+      scm {
+        gitSCM {
+          userRemoteConfigs {
+            userRemoteConfig {
+              credentialsId('shridharMe')
+              name('')
+              refspec('+refs/pull/*:refs/remotes/origin/pr/*')
+              url('git@github.com:shridharMe/rails-mysql-app.git')
+            }
+          }
+          branches {
+            branchSpec {
+              name('*/master')
+            }
+          }
+          browser {
+            gitWeb {
+              repoUrl('')
+            }
+          }
+          gitTool('')
+          doGenerateSubmoduleConfigurations(false)
+        }
+      }
+      scriptPath('Jenkinsfile')
+      lightweight(true)
+    }
+  }
+}
