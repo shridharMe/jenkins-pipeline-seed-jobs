dbc31d3f9048bc7259417d4fb491c775c4edf6f6
Shridhar <36041359+shridharMe@users.noreply.github.com>
2021-12-08 09:12:57 +0000

    Update jenkinsPipelineWithLibraryPR.groovy

--- jenkinsPipelineWithLibraryPR.groovy
+++ jenkinsPipelineWithLibraryPR.groovy
@@ -7,7 +7,7 @@ pipelineJob('pr-jenkins-pipeline-with-library') {
           userRemoteConfigs {
             userRemoteConfig {
               credentialsId('guthub_repo_access')
-              name('jenkins')
+              name('origin')
               refspec('+refs/pull/*:refs/remotes/origin/pr/*')
               url('git@github.com:shridharMe/jenkins-pipeline-with-library.git')
             }
