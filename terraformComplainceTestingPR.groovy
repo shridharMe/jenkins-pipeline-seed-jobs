aa5b01d6ff6c03850b6ed23b96e692307d41b484
Shridhar <36041359+shridharMe@users.noreply.github.com>
2021-12-14 13:45:46 +0000

    Update terraformComplainceTestingPR.groovy

--- terraformComplainceTestingPR.groovy
+++ terraformComplainceTestingPR.groovy
@@ -57,7 +57,7 @@ pipelineJob('pr-terraform-complaince-testing') {
             whiteListLabels("")
             includedRegions("")
             excludedRegions("")
-            triggerPhrase('restest this please')
+            triggerPhrase('retest this please')
             
 
             extensions {
